92a99f6f412bdc1653d4b35d6403602e85ee2464
Chantelle <Chantelle>
2016-03-31 16:35:35 +0200

    heyllo

--- classes/arrival.php
+++ classes/arrival.php
@@ -1,63 +1,72 @@
 <?php
-class FlightData{
-  // Variables being collected from the flight data API
-  public function getArrivalTime() {
-      return $this->arrivalTime;
-  }
 
-  public function getDepartureTime() {
-      return $this->departureTime;
-  }
+class FlightData {
 
-  public function getFlightName() {
-      return $this->flightName;
-  }
+    // Variables being collected from the flight data API
+    public function getArrivalTime() {
+        return $this->arrivalTime;
+    }
 
-  public function getFlightNumber() {
-      return $this->flightNumber;
-  }
+    public function getDepartureTime() {
+        return $this->departureTime;
+    }
 
-  public function getAirportOrigin() {
-      return $this->airportOrigin;
-  }
+    public function getFlightName() {
+        return $this->flightName;
+    }
 
-  public function setArrivalTime($arrivalTime) {
-      $this->arrivalTime = $arrivalTime;
-  }
+    public function getFlightNumber() {
+        return $this->flightNumber;
+    }
 
-  public function setDepartureTime($departureTime) {
-      $this->departureTime = $departureTime;
-  }
+    public function getAirportOrigin() {
+        return $this->airportOrigin;
+    }
 
-  public function setFlightName($flightName) {
-      $this->flightName = $flightName;
-  }
+    public function setArrivalTime($arrivalTime) {
+        $this->arrivalTime = $arrivalTime;
+    }
 
-  public function setFlightNumber($flightNumber) {
-      $this->flightNumber = $flightNumber;
-  }
+    public function setDepartureTime($departureTime) {
+        $this->departureTime = $departureTime;
+    }
 
-  public function setAirportOrigin($airportOrigin) {
-      $this->airportOrigin = $airportOrigin;
-  }
+    public function setFlightName($flightName) {
+        $this->flightName = $flightName;
+    }
 
-  
-  public function __construct($year, $month, $day, $hour){
-    $data = file_get_contents("https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/LUN/arr/$year/$month/$day/$hour?appId=f2fada9e&appKey=b3b6ad43212e524752691e4f5e2496ff&utc=false&numHours=5&codeType=FS&maxFlights=8");
-  }
-}
+    public function setFlightNumber($flightNumber) {
+        $this->flightNumber = $flightNumber;
+    }
 
-class GetTime{
-  public $day;
-  public $date;
-  public $year;
-  public $hour;
-  public $time;
+    public function setAirportOrigin($airportOrigin) {
+        $this->airportOrigin = $airportOrigin;
+    }
 
-  public function __construct(){
-    date_default_timezone_set("Africa/Lusaka");
-    $this->time = time();
-  }
+    public function __construct($year, $month, $day, $hour) {
+        $data = file_get_contents("https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/LUN/arr/$year/$month/$day/$hour?appId=f2fada9e&appKey=b3b6ad43212e524752691e4f5e2496ff&utc=false&numHours=5&codeType=FS&maxFlights=8");
+    }
+    //end of variables 
 
 }
+
+class GetTime {
+
+    public $day;
+    public $month;
+    public $year;
+    public $hour;
+    public $time;
+
+    public function __construct() {
+        date_default_timezone_set("Africa/Lusaka");
+        $this->time = time();
+        $this->day = date("d");
+        $this->year = date("Y");
+        $this->hour = date("H");
+        $this->month = date("m");
+    }
+
+}
+
 ?>
