918d09814d2dbbc396f6b2aa5f408c0e5f26fc69
Mwiza Benjamin Simbeye II <mwizasimbeye@gmail.com>
2016-03-31 17:05:50 +0200

    Create connect.php

---
+++ classes/connect.php
@@ -0,0 +1,29 @@
+<?php
+class connectCloud{
+  //Database connection variables
+  private $database;
+  private $hostname;
+  private $username;
+  private $password;
+
+  //MySQL variables
+  public #c
+
+  public function __construct($hostname, $username, $password, $database){
+    $this->username = $username;
+    $this->password = $password;
+    $this->hostname = $hostname;
+    $this->database = $database;
+    global $database;
+    global $hostname;
+    global $password;
+    global $username;
+
+
+  }
+
+  private function connect(){
+
+  }
+}
+?>
