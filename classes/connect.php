709cf61b5ddaf3a1d0d2ca07dfdc1e7bff2fe3fa
Mwiza Benjamin Simbeye II <mwizasimbeye@gmail.com>
2016-04-01 12:14:58 +0200

    Sorry for the late push

--- classes/connect.php
+++ classes/connect.php
@@ -5,6 +5,8 @@ class connectCloud{
   private $hostname;
   private $username;
   private $password;
+  public $time;
+  public $con;
 
   //MySQL variables
 
@@ -13,34 +15,44 @@ class connectCloud{
     $this->password = $password;
     $this->hostname = $hostname;
     $this->database = $database;
+
     try {
-        $con = new PDO('mysql:host='.$this->hostname.'; dbname='.$this->database, $this->username, $this->password);
-        $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $this->con = new PDO('mysql:host='.$this->hostname.'; dbname='.$this->database, $this->username, $this->password);
+        $this->con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         echo "Connection Successful";
-        $con->exec("SET CHARACTER SET utf8");  //  return all sql requests as UTF-8
+        $this->con->exec("SET CHARACTER SET utf8");  //  return all sql requests as UTF-8
     }
     catch (PDOException $err) {
         echo "Database connectin failed! Contact server admin!";
         $err->getMessage() . "<br/>";
-        file_put_contents('errorsCon.txt',$err, FILE_APPEND);  // write some details to an error-log outside public_html
         die();  //  terminate connection
     }
   }
 
   public function insertData($time){
-      global $this->time = $time;
+      $this->time = $time;
       //SQL Query
-      $insert = $con->prepare("INSERT INTO flights (id, time) VALUES (0, :time)");
+      $insert = $this->con->prepare("INSERT INTO time (id, time) VALUES (0, :time)");
       $insert->bindParam(':time', $this->time); //Insert Data
       $insert->execute();
   }
 
   public function readData(){
-
+    $this->time = $time;
+    //SQL Query
+    $read = $this->con->prepare("SELECT * FROM time WHERE id = :id");
+    $read->bindValue(':id', 0);
+    $read->execute();
+    $lasttime = $read->fetchObject();
+    echo $read->time;
   }
 
   public function updateData(){
 
   }
 }
+
+//Test area [CAUTION HARZARDS AHEAD]
+$connection = new connectCloud('localhost', 'root', 'pynch2015', 'flights');
+$connection->insertData('12552525252');
 ?>
